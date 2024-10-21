f5756fcb4e5bf63bfb8ae2b1f2f349de8ace0377
Josue <jecheverria@estudiantec.cr>
2024-10-20 22:55:41 -0600

    Hot-fix

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 
 // Iniciar servidor y sincronizar modelos
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
